95fa4ba12efd033f487a12e5403df1cb5a99160c
„Laimonas <laimonas.lukosevicius@gmail.com>
2021-09-23 20:36:58 +0300

    Add Posts and Home with Css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import TopBar from "./components/topbar/Topbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
      
      <TopBar />
+     <Home/> 
     </div>
   );
 }
